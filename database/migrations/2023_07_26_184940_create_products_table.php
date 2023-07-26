1b16eaf2d1320f0b728d9f75ccf4a2871cdc8219
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-26 16:00:00 -0300

    add uuid no produto e categoria

--- database/migrations/2023_07_26_184940_create_products_table.php
+++ database/migrations/2023_07_26_184940_create_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->unsignedBigInteger('tenant_id')->index();
             $table->unsignedBigInteger('store_id')->index();
 
