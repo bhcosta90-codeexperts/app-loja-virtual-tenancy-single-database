5ffccc6ee6a65cbf9a855a4e630c45bbce68d7a7
Bruno Costa <bruno.costa@dafiti.com.br>
2023-07-26 15:57:10 -0300

    add algumas migrações

--- database/migrations/2023_07_26_184839_create_stores_table.php
+++ database/migrations/2023_07_26_184839_create_stores_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('tenant_id')->index()->nullable();
-            $table->unsignedBigInteger('store_id')->index()->nullable();
+            $table->unsignedBigInteger('tenant_id')->index();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('cover')->nullable();
+
+            $table->string('subdomain')->unique();
+
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stores');
     }
 };
--- database/migrations/2023_07_26_184940_create_products_table.php
+++ database/migrations/2023_07_26_184940_create_products_table.php
@@ -11,15 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('tenant_id')->index()->nullable();
-            $table->unsignedBigInteger('store_id')->index()->nullable();
+            $table->unsignedBigInteger('tenant_id')->index();
+            $table->unsignedBigInteger('store_id')->index();
+
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description')->nullable();
+            $table->text('body');
+            $table->integer('price');
+            $table->string('image')->nullable();
+            $table->string('slug');
+
             $table->timestamps();
         });
     }
@@ -29,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
